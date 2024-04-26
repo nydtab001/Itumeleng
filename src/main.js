c0e56c2a8bca82dc8aba0deaa9996dbc6a54f4b5
nydtab001 <nydtab001@myuct.ac.za>
2024-04-26 12:18:58 +0200

    added contact.html and eddited styles.css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,98 @@
 const hambut = document.getElementById("hamburg");
 const nav = document.getElementById("navlist");
+const navdiv = document.querySelectorAll(".navdiv");
+const navlinks = document.querySelectorAll(".navbar li a");
 
 hambut.addEventListener("click", toggleButton)
 
 function toggleButton(){
     nav.classList.toggle("show")
 }
+
+navdiv.forEach((n) => n.addEventListener("click", clicked(n)));
+
+function clicked(element) {
+//  element.style.backgroundColor = "white";
+  pagecolor(element);
+}
+
+navdiv.forEach((n) => n.addEventListener("mouseover", () => buttonhover(n)));
+
+function buttonhover(element) {
+  element.style.backgroundColor = document.querySelector(".contact-info").style.backgroundColor;
+  element.style.color = "white";
+}
+
+function pagecolor(element) {
+  switch (element) {
+    case navdiv[0]:
+      element.style.backgroundColor = "rgb(50,50,255)"; // blue
+      document.querySelector(".contact-info").style.backgroundColor = "rgba(50,50,255, 0.9)"
+      break;
+    case navdiv[1]:
+      element.style.backgroundColor = "rgb(245, 60, 60)"; // red
+      document.querySelector(".contact-info").style.backgroundColor = "rgba(245,60,60, 0.9)"
+      break;
+    case navdiv[2]:
+      element.style.backgroundColor = "rgb(255, 190, 50)"; // yellow
+      document.querySelector(".contact-info").style.backgroundColor = "rgba(255,190,50, 0.9)"
+      break;
+    case navdiv[3]:
+      element.style.backgroundColor = "rgb(50,230,50)"; // green
+      document.querySelector(".contact-info").style.backgroundColor = "rgba(50,230,50, 0.9)"
+      break;
+  /*  case navdiv[4]:
+      element.style.backgroundColor = "navy";
+      break;
+    case navdiv[5]:
+      element.style.backgroundColor = "navy";
+      break;*/
+  }
+  element.style.color = "white";
+}
+
+navdiv.forEach((n) => n.addEventListener('mouseout', () => {
+  // Remove the hover effect, revert to the original style
+  setActiveLink(); // Sets it back to default
+ // n.style.color = 'navy';
+}))
+
+function resetBackgroundColors() {
+  navdiv.forEach((n) => {
+    n.style.backgroundColor = "";
+  });
+}
+
+// Call the setActiveLink function when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", setActiveLink);
+
+
+// Function to set active link based on current page URL
+function setActiveLink() {
+  const currentpage = window.location.href;
+  var normalizedCurrentPage = currentpage.replace(/index.html$/, ''); 
+  normalizedCurrentPage = normalizedCurrentPage.replace(/\/$/, "");
+//  console.log(normalizedCurrentPage);
+
+  navlinks.forEach((link) => {
+    var normalizedHref = link.href.replace(/index.html$/,"")
+    normalizedHref = normalizedHref.replace(/\/$/, "");
+    const renormalizedHref = normalizedHref.replace(/.html$/, "");
+ //   console.log(normalizedHref);
+    if (normalizedHref === normalizedCurrentPage) {
+      // Set background color of parent div
+     // link.querySelector(".navdiv").style.backgroundColor =
+     //   "navy";
+      pagecolor(link.querySelector(".navdiv"))
+      link.querySelector(".navdiv").style.color = "white";
+        
+    } else {
+      link.querySelector(".navdiv").style.backgroundColor = "";
+      link.querySelector(".navdiv").style.color = "navy";
+    }
+  });
+}
+
 
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev-btn');
